eee160f86d8bbb5c812c39f01c77ae0f067fc31e
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-07 01:48:17 +0200

    refactor: enhance Navbar component with improved styling

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -22,14 +22,14 @@ export default function Navbar() {
               <input
                 type="text"
                 placeholder="Search..."
-                className="bg-green-50 border-0 rounded px-4 py-2 pl-10 w-64 text-black  focus:outline-none focus:ring-2 focus:ring-green-500"
+                className="bg-green-200 border-0 rounded px-4 py-2 pl-10 w-64 text-black  focus:outline-none focus:ring-2 focus:ring-green-500"
               />
               <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
             </div>
-            <button className="p-2 bg-green-50 rounded hover:bg-green-100">
+            <button className="p-2 bg-green-200 rounded hover:bg-green-100">
               <User className="h-5 w-5 text-gray-700" />
             </button>
-            <button className="p-2 bg-green-50 rounded hover:bg-green-100">
+            <button className="p-2 bg-green-200 rounded hover:bg-green-100">
               <ShoppingCart className="h-5 w-5 text-gray-700" />
             </button>
           </div>
