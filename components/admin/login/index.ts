9c53e555edad1e26f184a3af6650e7e0135c8138
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-07 03:23:46 +0200

    chore: add login components barrel exports

---
+++ components/admin/login/index.ts
@@ -0,0 +1,6 @@
+export { default as LoginForm } from "./LoginForm";
+export { default as LoginHeader } from "./LoginHeader";
+export { default as LoginBackground } from "./LoginBackground";
+export { default as LoginFormFields } from "./LoginFormFields";
+export { default as LoginButton } from "./LoginButton";
+export { default as ErrorMessage } from "./ErrorMessage";
