d9a2c4e4f897ec72ad8435e129918ef9e65557f2
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-07 03:23:54 +0200

    chore: add add-product components barrel exports

---
+++ components/admin/add-product/index.ts
@@ -0,0 +1,3 @@
+export { default as AddProductForm } from "./AddProductForm";
+export { default as ProductFormFields } from "./ProductFormFields";
+export { default as ProductFormActions } from "./ProductFormActions";
