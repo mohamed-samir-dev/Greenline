faaef0491c6f5e18cd7edd712044454c864c5fc1
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-10 19:39:40 +0200

    feat: add products-list index for centralized component exports

---
+++ components/products/products-list/index.ts
@@ -0,0 +1,5 @@
+export { ProductFilters } from './ProductFilters';
+export { ProductCard } from './ProductCard';
+export { ProductGrid } from './ProductGrid';
+export { ProductHeader } from './ProductHeader';
+export { ProductPagination } from './ProductPagination';
