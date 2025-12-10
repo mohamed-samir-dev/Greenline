6cc14d2eca28c720b14a81dff4065a49bf0d8c70
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-10 19:33:15 +0200

    feat: add form-fields index for centralized component exports

---
+++ components/admin/add-product/form-fields/index.ts
@@ -0,0 +1,4 @@
+export { BasicInfoFields } from './BasicInfoFields';
+export { ImageFields } from './ImageFields';
+export { DescriptionFields } from './DescriptionFields';
+export { TagsField } from './TagsField';
