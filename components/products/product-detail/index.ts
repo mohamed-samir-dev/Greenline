46171c11d7e9a245f6369a3a9986cb00198c3577
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-12-10 19:37:22 +0200

    feat: add product-detail index for centralized component exports

---
+++ components/products/product-detail/index.ts
@@ -0,0 +1,7 @@
+export { ProductImageGallery } from "./ProductImageGallery";
+export { ProductInfo } from "./ProductInfo";
+export { ProductSpecifications } from "./ProductSpecifications";
+export { ProductUsageGuide } from "./ProductUsageGuide";
+export { ReviewForm } from "./ReviewForm";
+export { ReviewsList } from "./ReviewsList";
+export { ProductTabs } from "./ProductTabs";
